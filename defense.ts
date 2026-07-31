6c4ffb8200f7a5f6c34c579522b22d4846bf09d7
Jean Pierre Cedeño Andrade <jean.cedenoandrade5763@upse.edu.ec>
2026-08-01 05:41:08 +0800

    Delete defense.ts

--- defense.ts
+++
@@ -1,7 +0,0 @@
-namespace RobotSoccer {
-    export class DefenseStrategy implements Strategy {
-        run(snapshot: SensorSnapshot, movement: Movement) {
-            movement.stop()
-        }
-    }
-}

e0edc7d73208df8398f934909cac024d72605073
Jean Pierre Cedeño Andrade <jean.cedenoandrade5763@upse.edu.ec>
2026-08-01 05:40:28 +0800

    Delete recovery.ts

--- recovery.ts
+++
@@ -1,7 +0,0 @@
-namespace RobotSoccer {
-    export class RecoveryStrategy implements Strategy {
-        run(snapshot: SensorSnapshot, movement: Movement) {
-            movement.reverse()
-        }
-    }
-}
